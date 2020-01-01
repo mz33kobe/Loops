fcdfaa9f6902cc269d97cf31d4c820794b13e60f
Ndekere254 <ndekerekiritu@gmail.com>
2020-01-01 14:47:12 +0300

    Loops in C++

---
+++ index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	int areYouAhacker = 0;
+	areYouAhacker = areYouAhacker + 9;
+
+	while (areYouAhacker == 9) {
+		cout << "Did you get the Joke?!! \n";
+
+	}
+}
